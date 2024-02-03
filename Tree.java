103dc7b03cc46cabe2bd74d6cebb0bde503fc6f4
heliamazinani <123804298+heliamazinani@users.noreply.github.com>
2024-02-03 16:52:04 +0330

    tree

--- Tree.java
+++ Tree.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Tree {
     int depth;
     Node root=null;
-    MArrayList<Integer> maxd = new MArrayList<>();
+    MArrayList<Integer> maxDepth = new MArrayList<>();
     int count=0,count1=0,count2=0;
     public Tree(){
         this.depth = 0;
@@ -11,12 +11,40 @@ public class Tree {
     int getDepth(){
         return depth;
     }
-    Node creatTree(float[][] data, float[] labels) {
+    public Node createTree(float[][] data, float[] labels) {
         creatTree2(root,data,labels);
         return root;
     }
-    private Node creatTree2(Node node,float[][] data,float[] lables){
+    public Node creatTree2(Node node,float[][] data,float[] lables){
         Node p = null;
+        if(iGain(data,HighestInformationGain(data,lables),lables) > 0){
+            p = new Node(HighestInformationGain(data,lables),data,lables);
+            node =p;
+            if(root == null){
+                root = p;
+                p.depth =0;
+                maxDepth.add(0);
+            }
+            else{
+                p.depth = node.depth;
+            }
+            System.out.println("Feature Index: " + HighestInformationGain(data,lables));
+            float[] sort = Sort(p.data, p.getFeatureIndex(),p.data.length);
+            for (int i = 0 ; i < sort.length;i++){
+                split(p,sort[i]);
+                Node child =  p.children.get(i);
+//                System.out.println("display");
+//                child.display();
+                child.setDepth(p.depth+1);
+//                System.out.println("leaf" + p.isLeaf);
+                maxDepth.add(child.depth);
+//                System.out.println("depth" + child.depth);
+                if(Entropy(child.data, child.getFeatureIndex(), -1, child.lables) > 0 && Entropy(child.data, child.getFeatureIndex(), -1, child.lables) < 1){
+                    creatTree2(child,child.data,child.lables);
+                }
+            }}
+        //display();
+        return  p ;
 //        if(node!=null && node.lables.length ==  2 && (node.lables[0]!=node.lables[1])){
 //            float[][] kh2 = new float[1][ node.data[0].length] ;
 //            float[][] kh3 = new float[1][ node.data[0].length] ;
@@ -42,42 +70,12 @@ public class Tree {
 //            return node;
 //
 //        }
-
-        if(iGain(data,HighestInformationGain(data,lables),lables) > 0){
-
-         p = new Node(HighestInformationGain(data,lables),data,lables);
-
-            if(root == null){
-                root = p;
-                p.depth =0;
-                maxd.add(0);
-            }
-            else{
-                p.depth = node.depth;
-            }
-
-        System.out.println("F "+HighestInformationGain(data,lables));
-        float[] sort = Sort(p.data, p.getFeatureIndex(),p.data.length);
-        for (int i = 0 ; i < sort.length;i++){
-            split(p,sort[i]);
-            Node child =  p.children.get(i);
-            child.setDepth(p.depth+1);
-            maxd.add(child.depth);
-            System.out.println("depth" + child.depth);
-            if(Entropy(child.data, 0,-1, child.lables) > 0 && Entropy(child.data, 0,-1, child.lables) < 1){
-                creatTree2(child,child.data,child.lables);
-            }
-        }}
-        //display();
-        return  p ;
-
-
     }
     public int findDepth(){
-        int indexmax = maxd.get(0);
-        for(int i = 1 ; i < maxd.size(); i++ ){
-            if(maxd.get(i) >indexmax)
-                indexmax = maxd.get(i);
+        int indexmax = maxDepth.get(0);
+        for(int i = 1; i < maxDepth.size(); i++ ){
+            if(maxDepth.get(i) >indexmax)
+                indexmax = maxDepth.get(i);
         }
         return indexmax;
 
@@ -112,7 +110,6 @@ public class Tree {
             for(int i=0 ; i< tdata[0].length ; i++){
                 kh2[0][i] = tdata[0][i];
                 kh3[0][i] = tdata[1][i];
-
             }
             float[] kh = new float[1];
             kh[0] = tlabel[0];
@@ -120,7 +117,7 @@ public class Tree {
             parent.addchild(control,kh2,kh);
              child1 =parent.children.get(0);
             child1.setDepth(parent.depth+2);
-            maxd.add(child1.depth);
+            maxDepth.add(child1.depth);
 //            System.out.println("control bargh : "+control + "data split " +Arrays.deepToString(child1.data));
 //            for(int i=0; i<child1.lables.length ; i++){
 //                System.out.println(tlabel[i]);
@@ -128,23 +125,26 @@ public class Tree {
             kh[0] = tlabel[1];
             parent.addchild(control,kh3,kh);
              child2 =parent.children.get(1);
-            maxd.add(child2.depth);
+            maxDepth.add(child2.depth);
 //            System.out.println("control bargh: "+control + "data split " +Arrays.deepToString(child2.data));
 //            for(int i=0; i<child2.lables.length ; i++){
 //                System.out.println(child2.lables[i]);
 //            }
         }
+//        if(Entropy(tdata, parent.getFeatureIndex(),-1,tlabel) == 0){
+//            parent.setLeaf(true);
+//        }
         parent.addchild(control,tdata,tlabel);
-        System.out.println("control : "+control + "data split " +Arrays.deepToString(tdata) + "attribute " + parent.children.get((int)control).getFeatureIndex());
+//        System.out.println("control : "+control + "data split " +Arrays.deepToString(tdata) + "attribute " + parent.children.get((int)control).getFeatureIndex() + " E " + Entropy(data, parent.getFeatureIndex(),-1,label));
         for(int i=0; i<tlabel.length ; i++){
             System.out.println(tlabel[i]);
         }
         if(child1 != null && child2!= null){
-        System.out.println("control bargh : "+control + "data split " +Arrays.deepToString(child1.data) + "attribute: " + child1.getFeatureIndex());
+//        System.out.println("control bargh : "+control + "data split " +Arrays.deepToString(child1.data) + "attribute: " + child1.getFeatureIndex() + " leaf " + child1.isLeaf() + " E " + Entropy(tdata, child1.getFeatureIndex(),-1,tlabel));
         for(int i=0; i<child1.lables.length ; i++){
             System.out.println(child1.lables[i]);
         }
-            System.out.println("control bargh: "+control + "data split " +Arrays.deepToString(child2.data)+ "attribute: " + child2.getFeatureIndex());
+//            System.out.println("control bargh: "+control + "data split " +Arrays.deepToString(child2.data)+ "attribute: " + child2.getFeatureIndex() + " leaf " + child2.isLeaf() + " E " + Entropy(tdata, child2.getFeatureIndex(),-1,tlabel));
             for(int i=0; i<child2.lables.length ; i++){
                 System.out.println(child2.lables[i]);
             }
@@ -153,23 +153,24 @@ public class Tree {
         displayRecursively(root, 0);
     }
     private void displayRecursively(Node node, int depth) {
+        System.out.println("size" + node.children.size());
         if (node != null) {
             //System.out.println("khkhkh"+ node.children.get(0));
             StringBuilder indentation = new StringBuilder();
             for (int i = 0; i < depth; i++) {
-                indentation.append("  ");
+                indentation.append(" <> ");
             }
             int featureIndex = node.getFeatureIndex();
             float featureValue = node.getValue();
 
-            System.out.println(indentation + " Feature: " + featureIndex + ", Value: " + featureValue+
-                    "  att: " + Main.attributes[featureIndex]);
+            System.out.println(indentation + " Feature: " + featureIndex + ", Value: " + featureValue + "  att: " + Main.attributes[featureIndex]);
             for (int i = 0; i < node.children.size(); i++) {
                 System.out.println("bache");
                 displayRecursively(node.children.get(i), depth + 1);
             }
         }
     }
+
    public int HighestInformationGain(float[][] data,float[] labels){
         int indexmax = 0;
         float igain0 = iGain(data,0,labels);
